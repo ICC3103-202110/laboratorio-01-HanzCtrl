748b7972abb1df7c0673c2285e6973000e209964
HanzCtrl <75079296+HanzCtrl@users.noreply.github.com>
2021-03-22 16:33:06 -0300

    Primero

---
+++ temp.py
@@ -0,0 +1,63 @@
+#Super Lab1 de POO
+import numpy as np
+
+def menu():
+    print("-------------------------------------------------------------------------\n"
+          "Bienvenido al juego de memorice por favor indique la cantidad de pares \n"
+          "-------------------------------------------------------------------------") 
+    return
+
+def tablero(x):
+    
+
+menu()
+Pares = input("Pares?")
+print(Pares)
+
+Dimension = 2*Pares
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
