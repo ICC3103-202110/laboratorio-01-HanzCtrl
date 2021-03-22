cb9f0893e76582b4082a24d9af6d69c0883904d1
HanzCtrl <75079296+HanzCtrl@users.noreply.github.com>
2021-03-22 17:12:26 -0300

    Segundo

--- temp.py
+++ temp.py
@@ -7,14 +7,25 @@ def menu():
           "-------------------------------------------------------------------------") 
     return
 
-def tablero(x):
+def tablero():
+    menu()
+    Pares = int(input("Pares? "))
+    if Pares <= 0:
+        print("Se necesita al menos 1 par para poder jugar!\n")
+        tablero()
+    else:
+        Dimension = Pares * 2
+        
+        
+             
+              
+    return
     
 
-menu()
-Pares = input("Pares?")
-print(Pares)
 
-Dimension = 2*Pares
+
+
+
 
 
 
